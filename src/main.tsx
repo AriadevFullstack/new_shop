9de18d1476c8a3893159983f482e1ceeccfd94f8
Ariadesigner <m.sadegh.noruzi7165@gmail.com>
2025-05-10 08:11:28 -0700

    feat: routing add using react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import { BrowserRouter, RouterProvider } from "react-router";
+import { router } from "./routes/route.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
---
+++ src/routes/route.ts
@@ -0,0 +1,10 @@
+import {createBrowserRouter} from "react-router";
+import { Home } from "../pages/Home.page";
+  
+export  let router = createBrowserRouter([
+    {
+      path: "/",
+      Component: Home,
+      
+    },
+  ]);
