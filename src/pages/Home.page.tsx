93a3ec5a8b9199246f5732a54705c9ffd3885dc8
Ariadesigner <m.sadegh.noruzi7165@gmail.com>
2025-05-14 10:11:38 -0700

    fix: fix product card props bug

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -9,13 +9,13 @@ export const Home = () => {
     <main className="bg-white">
       <ProductSection title="New Arrivals" buttonText="View All">
         {products.slice(0, 4).map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.id} {...product} />
         ))}
       </ProductSection>
 
       <ProductSection title="Top Selling" buttonText="View All">
         {products.slice(4, 8).map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.id} {...product} />
         ))}
       </ProductSection>
     </main>
