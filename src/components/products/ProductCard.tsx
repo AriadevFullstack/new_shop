03cc5783f741a4f8cfda945c7a6b901f9a5bc584
Ariadesigner <m.sadegh.noruzi7165@gmail.com>
2025-05-16 08:49:07 -0700

    feat: add swiper js add to product section

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -14,25 +14,27 @@ export const ProductCard = ({
 
   return (
     <div className="flex items-center justify-center">
-      <div className="flex h-auto w-[295px] flex-col justify-between sm:h-[408px] sm:w-[48%] md:w-[295px]">
-        <div className="mb-4">
+      <div className="w-full max-w-[295px] flex flex-col justify-between">
+        
+        <div className="mb-3">
           <img
             src={productImage}
             alt={productName}
-            className="h-[298px] w-full rounded-xl object-cover"
+            className="h-[250px] sm:h-[298px] w-full rounded-xl object-cover"
           />
         </div>
 
+        
         <div className="line-clamp-2">
-          <h2 className="mb-2 h-[27px] text-[18px] font-semibold text-gray-900">
+          <h2 className="mb-2 text-base sm:text-[18px] font-semibold text-gray-900 leading-6">
             {productName}
           </h2>
 
-          <div className="mb-2 flex h-[20px] items-center gap-[4px]">
+          <div className="mb-2 flex items-center gap-1">
             {Array.from({ length: 5 }).map((_, i) => (
               <Star
                 key={i}
-                className={`h-5 w-5 ${
+                className={`h-4 w-4 sm:h-5 sm:w-5 ${
                   i < rate ? "text-yellow-400" : "text-gray-300"
                 }`}
                 fill={i < rate ? "#facc15" : "none"}
@@ -40,19 +42,20 @@ export const ProductCard = ({
             ))}
           </div>
 
-          <div className="mb-2 flex h-[32px] w-full items-center gap-2">
+         
+          <div className="flex flex-wrap items-center gap-2">
             {disCount > 0 ? (
               <>
                 <span className="text-sm text-gray-400 line-through">
                   {price.toLocaleString()} $
                 </span>
-                <span className="text-lg font-bold text-red-600">
+                <span className="text-base sm:text-lg font-bold text-red-600">
                   {finalPrice.toLocaleString()} $
                 </span>
                 <span className="text-sm text-green-600">{disCount}%</span>
               </>
             ) : (
-              <span className="text-lg font-bold text-gray-800">
+              <span className="text-base sm:text-lg font-bold text-gray-800">
                 {price.toLocaleString()} $
               </span>
             )}
@@ -62,3 +65,4 @@ export const ProductCard = ({
     </div>
   );
 };
+
--- src/components/products/ProductSection.tsx
+++ src/components/products/ProductSection.tsx
@@ -1,26 +1,54 @@
 import React from "react";
 import type { SectionProps } from "../../types/ProductType";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 export const ProductSection: React.FC<SectionProps> = ({
   title,
   buttonText,
   children,
 }) => {
   return (
-    <section className="bg-white px-4 py-12">
-      <div className="mb-10 text-center">
-        <h2 className="text-[32px] font-semibold text-gray-900">{title}</h2>
+    <section className="my-10">
+      <div className="mb-6 text-center">
+        <h2 className="text-xl font-bold">{title}</h2>
       </div>
 
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4">
-        {children}
+      <div className="relative">
+        <Swiper
+          modules={[Navigation]}
+          navigation
+          spaceBetween={16}
+          slidesPerView= {1.3}
+          breakpoints={{
+            480: {
+              slidesPerView: 2.3,
+            },
+            768: {
+              slidesPerView: 3.3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+          }}
+          className="!px-2"
+        >
+          {React.Children.map(children, (child, i) => (
+            <SwiperSlide key={i}>{child}</SwiperSlide>
+          ))}
+        </Swiper>
       </div>
 
-      <div className="mt-10 text-center">
-        <button className="rounded-md bg-black px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-gray-800">
+      <div className="mt-6 text-center">
+        <button className="px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-800 transition">
           {buttonText}
         </button>
       </div>
     </section>
   );
 };
+
